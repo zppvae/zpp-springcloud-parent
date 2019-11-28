154fa19c2e44ca62332e98ebec687dbbcd51b287
zhupp <zhupp@svocloud.com>
2019-11-28 16:39:47 +0800

    elasticsearch init

---
+++ zpp-springcloud-search/src/main/java/org/zpp/search/SearchApplication.java
@@ -0,0 +1,16 @@
+package org.zpp.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zpp
+ * @date 2019/11/28 15:29
+ */
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SearchApplication.class,args);
+    }
+}
